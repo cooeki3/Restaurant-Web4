167a11731c55de0a15a357c385a25ceba8167abc
Alexandre Gratton <6233423@cstj.qc.ca>
2025-03-18 00:26:39 -0400

    Modification du Nav + Ajout du panneau rétractable

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("nav-content").style.width = "100%";
+  }
+
+  function closeNav() {
+    document.getElementById("nav-content").style.width = "0%";
+  }
