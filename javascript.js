e68aed914c2c1a161a8b8ad89c43d8f97abe7d49
Alexandre Gratton <6233423@cstj.qc.ca>
2025-03-18 14:16:15 -0400

    Ajout transitions Nav

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
-function openNav() {
-    document.getElementById("nav-content").style.width = "100%";
-  }
+var navContent = document.getElementById("nav-content");
+var navLinks = document.getElementById("nav-links");
 
-  function closeNav() {
-    document.getElementById("nav-content").style.width = "0%";
-  }
+console.log(navLinks);
+
+function openNav() {
+  navContent.style.transition = "opacity 0.7s cubic-bezier(1,.17,.95,.86), width 0.7s ease-in-out";
+  navContent.style.width = "100%";
+  navLinks.style.opacity = "1";
+}
+
+function closeNav() {
+  navContent.style.transition = "opacity 0.7s cubic-bezier(.07,.96,.23,.9), width 0.7s ease-in-out";
+  navContent.style.width = "0%";
+  navLinks.style.opacity = "0";
+}
